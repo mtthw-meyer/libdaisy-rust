mtthw-meyer/libdaisy-rust

commit e7f515e60ae1ea856947f6d43059af09e6295b02

better audio freq accuracy and allow application-controlled logs (#33)
* log: new feature to disable panic handler

In some situations, the user of the library may want to use its
own panic handler. This is not possible currently, because panic_halt
is used by default when no log feature is enabled.

Add a new "log-none" feature that disables both logs and panic handler.

Signed-off-by: Olivier Matz <zer0@droids-corp.org>

* system: increase audio frequency accuracy

Currently, the effective audio frequency is:
  pll3_p_ck / 256 = 12329003 / 256 = 48160.168 Hz

This is not very accurate (0.33 % error). To enhance this, use a
fractional ratio for PLL3 configuration. After this change, the audio
frequency is:
  pll3_p_ck / 256 = 12287998 / 256 = 47999.992 Khz

The error is now 0.0000162 %, comparable to the precision of the
crystal.

Signed-off-by: Olivier Matz <zer0@droids-corp.org>

---------

Signed-off-by: Olivier Matz <zer0@droids-corp.org>
Co-authored-by: Olivier Matz <zer0@droids-corp.org>

src/system.rs
@@ -65,8 +65,8 @@ impl System {
             // .pll2_q_ck(PLL2_Q_HZ)
             // .pll2_r_ck(PLL2_R_HZ)
             // PLL3
-            .pll3_strategy(rcc::PllConfigStrategy::Iterative)
-            .pll3_p_ck(PLL3_P_HZ)
+            .pll3_strategy(rcc::PllConfigStrategy::Fractional)
+            .pll3_p_ck(PLL3_P_HZ) // used for SAI1
             .pll3_q_ck(PLL3_Q_HZ)
             .pll3_r_ck(PLL3_R_HZ)
             .freeze(vos, &syscfg)

Cargo.toml
@@ -35,6 +35,7 @@ default = []
 log-rtt = ["rtt-target", "panic-rtt-target"]
 log-itm = ["panic-itm", "lazy_static", "cortex-m-log"]
 log-semihosting = ["panic-semihosting", "lazy_static", "cortex-m-log", "cortex-m-semihosting"]
+log-none = []
 
 # this lets you use `cargo fix`!
 #[[bin]]

src/logger.rs
@@ -85,6 +85,10 @@ cfg_if::cfg_if! {
             cortex_m_log::log::init(&LOGGER).unwrap();
         }
     }
+    else if #[cfg(any(feature = "log-none"))] {
+        /// no log and no panic handler
+        pub fn init() {}
+    }
     else {
         use panic_halt as _;
         /// Initialize logging if feature is enabled, otherwise does nothing

commit b6967da2968903ac88bf4b2b720c9c8a8cf01d65

system: enhance precision of PLL configuration
In commit e7f515e60ae1 ("better audio freq accuracy and allow
application-controlled logs (#33)"), the PLL configuration was updated
to provide a better precision, but this was incorrect.

The master clock is supposed to be 256 times the frequency sampling,
but a factor of 257 was used to ensure the frequency is above. Using
PllConfigStrategy::Fractional was then useless.

Change the factor to 256 and use PllConfigStrategy::FractionalNotLess to
keep the constraint of generating a clock higher than the target. From
init_clocks() log, the PLL3_P clock is 12288001, which is 0.000008 %
above the right frequency (12288000).

Link: https://www.st.com/content/ccc/resource/training/technical/product_training/group0/81/96/f3/1a/55/d9/40/52/STM32H7-System-Reset_and_clock_control_RCC/files/STM32H7-System-Reset_and_clock_control_RCC.pdf/_jcr_content/translations/en.STM32H7-System-Reset_and_clock_control_RCC.pdf
Link: https://www.st.com/content/ccc/resource/training/technical/product_training/group0/d3/c0/b0/0e/fe/eb/40/a9/STM32H7-Peripheral-Serial-Audio-Interface_SAI/files/STM32H7-Peripheral-Serial-Audio-Interface_SAI.pdf/_jcr_content/translations/en.STM32H7-Peripheral-Serial-Audio-Interface_SAI.pdf
Fixes: e7f515e60ae1 ("better audio freq accuracy and allow application-controlled logs (#33)")

Signed-off-by: Olivier Matz <zer0@droids-corp.org>

src/system.rs
@@ -32,7 +32,7 @@ const PLL1_R_HZ: Hertz = Hertz::from_raw(CLOCK_RATE_HZ.raw() / 32);
 // PLL2
 const PLL2_P_HZ: Hertz = Hertz::from_raw(4_000_000);
 
-const PLL3_P_HZ: Hertz = Hertz::from_raw(AUDIO_SAMPLE_HZ.raw() * 257);
+const PLL3_P_HZ: Hertz = Hertz::from_raw(AUDIO_SAMPLE_HZ.raw() * 256);
 
 pub struct System {
     pub gpio: crate::gpio::GPIO,
@@ -65,7 +65,7 @@ impl System {
             // PLL2
             .pll2_p_ck(PLL2_P_HZ) // Default adc_ker_ck_input
             // PLL3
-            .pll3_strategy(rcc::PllConfigStrategy::Fractional)
+            .pll3_strategy(rcc::PllConfigStrategy::FractionalNotLess)
             .pll3_p_ck(PLL3_P_HZ) // used for SAI1
             .freeze(vos, &syscfg)
     }
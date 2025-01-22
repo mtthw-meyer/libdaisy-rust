7d1b87364b00064f5b4b3ad084d2e24a5f098521
Matthew Meyer <mtthw.meyer@gmail.com>
2025-01-22 11:35:17 -0500

    Fix CI Issues (#42)
    * Updated MSRV to 1.70.0
    * Set usbd-midi to 0.3.0 to match when it was added
    * Updated logger to use HostStream for semihosting

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ cargo objcopy --example passthru --release -- -O binary passthru.bin
 [cargo-binutils-url]: https://github.com/rust-embedded/cargo-binutils
 
 # Minimum supported Rust version
-The Minimum Supported Rust Version (MSRV) at the moment is 1.68.2
+The Minimum Supported Rust Version (MSRV) at the moment is 1.70.0
 # Demos
 
 [Looper](https://github.com/mtthw-meyer/daisy-looper) - Basic one button looper.
--- examples/usb_midi.rs
+++ examples/usb_midi.rs
@@ -136,7 +136,8 @@ mod app {
         let midi = MidiClass::new(usb_bus, 1, 1).unwrap();
 
         let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x16c0, 0x5e4))
-            .strings(&[StringDescriptors::default().product("daisy midi")]).unwrap()
+            .strings(&[StringDescriptors::default().product("daisy midi")])
+            .unwrap()
             .device_class(USB_CLASS_NONE)
             .build();
 
